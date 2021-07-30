c873e92d21b265430e24ac4bb173ffef72d3afc1
john.charlie <johncharlie.bautista@amici.com.ph>
2021-07-30 11:28:02 +0800

    update

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
               </a>
               <div class="float-end">
                 <button class="btn " type="submit">I'm already a customer</button>
-                <button class="btn btn-outline-success" type="submit" style="margin-left: -.5rem;">Login</button>
+                <button class="btn btn-outline-success" type="submit" id="loginbtn">Login</button>
               </div>
           </div>
       </div>
@@ -31,25 +31,25 @@
         <div class="row">
             <div class="col col-md-3"></div>
             <div class="col-sm-12 col-md-6">
-              <img src="img/Crazy_Prices_LP.png" alt="logo" class="IMG" style="margin-left: -18rem">
-              <div style="margin-top: -38rem; margin-left: 11rem;">
-                <p class="text1" style="margin: 0 !important;">LIVERPOOL V AUGSBURG </p>
-                <p class="text2 yellow-text " style="margin: 0 !important;">LIVERPOOL TO WIN</p>
-                <p class="text3 yellow-text " style="margin: 0 !important;">iN 90 MINS</p>
-                <p class="text4 yellow-text " style="margin: 0 !important;">8/1</p>
+              <img src="img/Crazy_Prices_LP.png" alt="logo" class="IMG" >
+              <div class="box-middle">
+                <p class="text1" >LIVERPOOL V AUGSBURG </p>
+                <p class="text2 yellow-text " >LIVERPOOL TO WIN</p>
+                <p class="text3 yellow-text " >iN 90 MINS</p>
+                <p class="text4 yellow-text " >8/1</p>
                 <p class="text5">£/€ 5 Bet Only | PLUS £/€5 Free Bet should your bet lose</p>
               </div>
             </div>
         </div>
         <div class="row">
             <div class="col col-md-5"></div>
-            <div class="col-sm-12 col-md-3" style="margin-left: 3rem">
+            <div class="col-sm-12 col-md-3" id="email-col">
               <input class="form-control me-2" type="text" id="email" placeholder="Email" aria-label="Email">
               <div class="d-grid mt-2">
-                <div class="border3" style="min-width: inherit; min-height: inherit;">
-                  <div class="border2" style="min-width: inherit; min-height: inherit;">
+                <div class="border3" >
+                  <div class="border2" >
                   <div class="d-grid mt-2">
-                    <button class="btn" type="submit" id="betNow" style="min-width: inherit; min-height: inherit;">BET NOW</button>
+                    <button class="btn" type="submit" id="betNow" >BET NOW</button>
                   </div>
                   </div>
                 </div>
@@ -64,7 +64,7 @@
                   <h1 class="one align-middle">1</h1>
                 </div>
                 <div class="col-9">
-                  <p class="register align-middle" style=" margin-top: 1.5rem;">REGISTER & DEPOSIT £/€5 OR MORE</p>
+                  <p class="register align-middle">REGISTER & DEPOSIT £/€5 OR MORE</p>
                 </div>
               </div>
             </div>
@@ -74,7 +74,7 @@
                   <h1 class="two align-middle">2</h1>
                 </div>
                 <div class="col-9">
-                  <p class="enhance align-middle" style=" margin-top: 1.5rem;">YOUR ENHANCED ODDS WILL DISPLAY ON THE HOMEPAGE</p>
+                  <p class="enhance align-middle">YOUR ENHANCED ODDS WILL DISPLAY ON THE HOMEPAGE</p>
                 </div>
               </div>
             </div>
@@ -84,7 +84,7 @@
                   <h1 class="three align-middle">3</h1>
                 </div>
                 <div class="col-9">
-                  <p class="betWins align-middle" style=" margin-top: 1.5rem;">IF YOUR BET WINS YOUR WINNINGS WILL BE PAID AS A £/€40 FREE BET</p>
+                  <p class="betWins align-middle">IF YOUR BET WINS YOUR WINNINGS WILL BE PAID AS A £/€40 FREE BET</p>
                 </div>
               </div>
             </div>
@@ -92,15 +92,15 @@
         <div class="row">
           <div class="col col-sm-12 col-md-6"></div>
           <div class="col col-sm-12 col-md-3 text-center" style="margin-left: -3rem;">
-            <img src="img/layer.png" alt="" class="" style="max-width: inherit;">
+            <img src="img/layer.png" alt="" class="gambleware">
           </div>    
         </div>
         <div class="row">
           <div class="col-md-3"></div>
           <div class="col-sm-12 col-md-9 text-center mt-2" style="color: white; margin-left: -3rem;">
             <img src="img/arrow.png" alt="">
-            <a href="" style="color: white !important;">About Coral Sports Betting</a> | 
-            <a href="" style="color: white !important;">Terms & conditions</a>
+            <a href="" class="aboutUs">About Coral Sports Betting</a> | 
+            <a href="" class="termsnCon">Terms & conditions</a>
           </div>
         </div>
     </div>
